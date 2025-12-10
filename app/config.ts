a6241e6bb74cccfa0620f21bcf489abd2526baf2
PONG13202 <khunnongmai17915@gmail.com>
2025-12-10 22:53:22 +0700

    fix 3

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 export const config = {
   // ให้ใช้ค่าจาก Environment Variable ก่อน ถ้าไม่มี (เช่นตอนรันในเครื่อง) ค่อยใช้ localhost
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || "https://api-sailom.vercel.app",
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || "https://api-theta-eight-44.vercel.app",
 };
